30e05803a5dec122f8bcc69421529ddc3df709ce
Deepak Kumar <deepakkumar.ac.in@gmail.com>
2021-05-16 11:13:20 +0530

    updating our app with a shop page, collectio preview component and collection item component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
 import { Route, Link } from 'react-router-dom';
 import './App.css';
-import HomePage from './pages/homePage/homepage.component.jsx';
+import HomePage from './pages/homePage/homepage.component';
+import ShopPage from './pages/shop/shop.component'
 import './pages/homePage/homepage.styles.scss';
 
-const HatsPage = (props) => {
-  console.log(props);
-  return (
-    <div>
-      {/* <Link to='/topics'>Topics</Link> */}
-      <h1>Hats Page</h1>
-    </div>
-  )
-};
 
 function App() {
   return (
     <div>
       <Route exact path='/' component={HomePage} />
-      <Route path='/hats' component={HatsPage} />      
+      <Route path='/shop' component={ShopPage} />      
     </div>
   );
 }
